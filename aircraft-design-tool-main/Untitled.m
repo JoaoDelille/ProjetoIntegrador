7c532caf3b87ac317d46147194a97610ab795b07
JoanaMatos00 <joanafmatos@tecnico.ulisboa.pt>
2022-06-29 03:52:06 +0100

    cenas
    cenas

--- aircraft-design-tool-main/Untitled.m
+++ aircraft-design-tool-main/Untitled.m
@@ -1,5 +1,5 @@
 clc
 close all
-%adt('prototipo_vfinal.json','letext_joana2.json')
+adt('prototipo_vfinal.json','letext_joana2.json')
 %adt('letext.json')
-adt('prototipo_vfinal.json')
+%adt('prototipo_vfinal.json')
